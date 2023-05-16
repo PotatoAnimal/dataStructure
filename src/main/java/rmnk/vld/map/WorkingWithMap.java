89f9a4e745b88d3311397ab0e3ff12e508547941
PotatoAnimal <potatoanimals@gmail.com>
2023-05-16 17:25:17 +0300

    1. project structure & folders - done 2. arrays exmpls - done 3. 2DArrays exmpls - done 4. List N ArrayList<> - done 5. Stack - done 5. Queue - done 6. LinkList - done 7. Sets - done 8. Map N InterfaceMap - done

---
+++ src/main/java/rmnk/vld/map/WorkingWithMap.java
@@ -0,0 +1,24 @@
+package rmnk.vld.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WorkingWithMap {
+    public static void main(String[] args) {
+        Map<Integer, Person> map = new HashMap<>();
+
+        map.put(1, new Person("Vlad"));
+        map.put(2, new Person("Vlad"));
+        map.put(3, new Person("Roma"));
+        System.out.println(map);
+        System.out.println(map.entrySet());
+        System.out.println(map.keySet());
+        System.out.println(map.containsKey(3));
+        System.out.println(map.values());
+        System.out.println(map.remove(2));
+        System.out.println(map);
+        System.out.println(map.getOrDefault(2, new Person("def")));
+        System.out.println(map.size());
+    }
+    record Person(String s){}
+}
---
+++ src/main/java/rmnk/vld/mapInterface/Picture.java
@@ -0,0 +1,29 @@
+package rmnk.vld.mapInterface;
+
+public class Picture {
+    public static void main(String[] args) {
+        /*
+                                    MAP
+                                  -  -  -
+                                 -   -   -
+                                -    -    -
+                               -     -     -
+                              -      -      -
+                    HASH TABLE       -       - - - - - - SORTED MAP
+            (Synchronised)           -                       -
+                                  HASH MAP                   -
+                                     -                       -
+                                     -                 NAVIGATE TABLE
+                                     -                       -
+                                     -                       -
+                              LINKED HASH MAP                -
+                                                             -
+                          (Doubly LinkedListed         TREE MAP     (sorted)
+                          based impl)
+                          Slow
+
+@NotNull allowed in Hash Table and Tree map
+@Null allowed in HashMap and Linked hash map
+*/
+    }
+}
